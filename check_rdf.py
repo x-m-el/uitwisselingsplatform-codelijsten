e67fcc8b0858bec1f9bf31b6d2fe8d1ab8c1efd0
Ruben <ruben.de.facq@redpencil.io>
2024-01-02 13:32:51 +0100

    check rdf via SHACL file too

--- check_rdf.py
+++ check_rdf.py
@@ -1,14 +1,27 @@
 from rdflib import Graph
+from pyshacl import validate
 import os
-import pprint
 
 schemes_dir = './conceptschemes'
-g = Graph()
+
+def validate_shape(graph):
+    return validate(data_graph=graph, shacl_graph="shacl_codelists.ttl")
+    
+def validate_turtle(file_path):
+    try:
+        g = Graph()
+        g.parse(file_path, format="turtle")
+    except Exception as e:
+        print(f"Error validating {file_path}: {e}")
+        return False
+    (is_valid, _, failure_reason)=  validate_shape(g)
+    if is_valid:
+        return True
+    else:
+        print(f"Error validating shape {file_path}: {failure_reason}")
+        return False
+      
 for filename_codelist in os.listdir(schemes_dir):
   print("### Parsing file ", filename_codelist, "###")
-  g.parse(os.path.join(schemes_dir, filename_codelist))
-
-# for stmt in g:
-#   pprint.pprint(stmt)
-
-print("### All files parsed successfully ###")  
+  file = os.path.join(schemes_dir, filename_codelist)
+  validate_turtle(file)
